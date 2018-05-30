d869767cb294c14460b7655b3a5a5e5a889f8d19
umarfarooq63114 <39738247+umarfarooq63114@users.noreply.github.com>
2018-05-31 00:24:53 +0500

    recycler view tak

--- app/src/main/java/edmt/dev/androidgridlayout/MainActivity.java
+++ app/src/main/java/edmt/dev/androidgridlayout/MainActivity.java
@@ -1,23 +1,61 @@
 package edmt.dev.androidgridlayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     GridLayout mainGrid;
 
+    RecyclerView recyclerView;
+
+    RecyclerView.Adapter adapter;
+    ArrayList<Technician> own;
+    Button btnSave;
+    RecyclerView.LayoutManager layoutManager;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        own = new ArrayList<>();
+
+
+        //own.add(new User(R.drawable.umar,"umar","farooq"));
+        own.add(new Technician(R.drawable.facebook_avatar,"umar","00000", (float) 3.14));
+        own.add(new Technician(R.drawable.facebook_avatar,"umar","00000",(float) 3.0));
+        own.add(new Technician(R.drawable.facebook_avatar,"umar","00000",(float)3.3));
+        own.add(new Technician(R.drawable.facebook_avatar,"umar","00000",(float)3.2));
+        own.add(new Technician(R.drawable.facebook_avatar,"umar","00000",(float)4.5));
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new TechnicianAdapter(this,own);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+
+
+
+
 
         mainGrid = (GridLayout) findViewById(R.id.mainGrid);
 
@@ -58,10 +96,10 @@ public class MainActivity extends AppCompatActivity {
             cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
-                    Intent intent = new Intent(MainActivity.this,ActivityOne.class);
+                if( finalI == 0){
+                    Intent intent = new Intent(MainActivity.this,DeviceDetails.class);
                     intent.putExtra("info","This is activity from card item index  "+finalI);
-                    startActivity(intent);
+                    startActivity(intent);}
 
                 }
             });
